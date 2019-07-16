cc451ade3b4e95d71ad1fd320c86794f0aa088a3
Dmitriy <dmytro.shcherbakov@svitsoft.com>
2019-07-16 19:58:22 +0300

    Remove usings

--- BLL/Interfaces/ITradeMarkService.cs
+++ BLL/Interfaces/ITradeMarkService.cs
@@ -1,8 +1,5 @@
 ﻿using BLL.DTO;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Interfaces
--- BLL/Services/SizeService.cs
+++ BLL/Services/SizeService.cs
@@ -3,10 +3,7 @@ using BLL.DTO;
 using BLL.Interfaces;
 using DAL;
 using DAL.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Services
